aae73af11cb8f245daed02b2ed0c141d1bcafe50
Bernardo Quiroga <156685978+berquiroga@users.noreply.github.com>
2025-08-13 20:23:24 -0400

    Add preprocessing functions

--- dmd_forecast_crypto.py
+++ dmd_forecast_crypto.py
@@ -15,3 +15,48 @@ import shelve
 import matplotlib.pyplot as plt
 import matplotlib
 from matplotlib.patches import Rectangle
+
+#%%
+# Preprocessing functions
+# Data is already stored in a shelve. Function to obtain 1000 closing price data
+def get_timeframe_data(timeframe):
+    database = shelve.open("shelve_historical_data")
+    timeframe_symbols = database[f"{timeframe}_symbols"]
+    timeframe_data = database[timeframe]
+    return timeframe_data, timeframe_symbols
+
+
+# Retrieve only data for the symbols and timeframe wanted from the shelve
+def get_symbols_data(symbols, timeframe):
+    timeframe_historical_data, timeframe_symbols = get_timeframe_data(timeframe)
+    for i in range(len(symbols)):
+        index = timeframe_symbols.index(symbols[i])
+        if i == 0:
+            symbols_historical_data = timeframe_historical_data[index,:]
+        else:
+            symbols_historical_data = np.vstack((symbols_historical_data, timeframe_historical_data[index,:]))
+    return symbols_historical_data
+
+
+# Compute log returns for all columns on the numpy array
+def compute_log_returns(timeframe_data):
+    rows = timeframe_data.shape[0]
+    columns = timeframe_data.shape[1]
+    log_returns = np.zeros((rows, columns - 1))
+    for i in range(rows):
+        for j in range(columns - 1):
+            log_returns[i,j] = np.log(timeframe_data[i, j+1] / timeframe_data[i,j])
+    return log_returns
+
+
+# Compute the z-score for the log returns to normalize
+def get_z_scores(log_returns):
+    rows_log_returns = log_returns.shape[0]
+    columns_log_returns = log_returns.shape[1]
+    means = log_returns.mean(axis=1)
+    stds = log_returns.std(axis=1)
+    z_scores = np.zeros((rows_log_returns, columns_log_returns))
+    for i in range(rows_log_returns):
+        for j in range(columns_log_returns):
+            z_scores[i,j] = (log_returns[i,j] - means[i]) / stds[i]
+    return z_scores, means, stds
